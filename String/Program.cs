6f3a7c909f40eecc9cb732327ced84a614ffcc26
David Rodriguez <rodriguezd11@uabc.edu.mx>
2022-03-01 17:38:40 -0800

    Practica y reto con String

---
+++ String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pedir por consola el nombre de alguien y su cuidad y
+            //mostrarla por pantalla
+            string nombre, cuidad;
+            Console.WriteLine("Ingrese su nombre.");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su ciudad.");
+            cuidad = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + ", Bienvenido a " + cuidad);
+            Console.WriteLine("Presione una tecla para fianlizar...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ StringInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, edad;
+            Console.WriteLine("Ingrese su nombre.");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad.");
+            edad = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + " y tienes: " + edad + " años");
+            Console.WriteLine("Presione una tecla para fianlizar...");
+            Console.ReadKey();
+        }
+    }
+}
